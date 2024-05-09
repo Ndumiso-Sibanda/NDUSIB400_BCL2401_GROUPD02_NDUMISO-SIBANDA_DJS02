5fde40fd09929e81bcf3a364f16e5b316d6354d0
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-09 14:23:47 +0200

    Updated JS

--- scripts.js
+++ scripts.js
@@ -23,11 +23,30 @@ form.addEventListener("submit", (event) => {
  // Check for division by zero
  if (divider == 0) {
   result.innerText =
-   "Division by zero is not allowed. Please enter a valid divider.";
+   "Division not performed. Invalid number provided. Try again.";
   result.classList.add("error-message");
+  console.error(
+   "Division by zero error. Dividend:",
+   dividend,
+   "Divider:",
+   divider
+  );
   return;
  }
+
  // Perform the division
  result.innerText = Math.floor(dividend / divider);
  result.classList.remove("error-message");
 });
+
+/**###Scenario: Providing anything that is not a number should crash the program
+
+- GIVEN that the submit button is pressed
+- WHEN 'any key that is not a number’ is entered into the first input
+- AND ‘ any key that is not a number’ is entered into the second input.
+AND again if 'any key that is not a number’ is entered into the first input
+THEN any number is entered
+AND again if 'any number is entered’ into the first input
+THEN 'any key that is not a number' is entered
+- THEN the entire screen should be replaced with “Something critical went wrong. Please reload the page
+**/
